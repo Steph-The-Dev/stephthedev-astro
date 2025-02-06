2008b0d823f1e9e4d78ea42db93852c39c398ffb
Steph-The-Dev <69237947+Steph-The-Dev@users.noreply.github.com>
2025-02-06 10:40:13 +0100

    added robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,18 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    icon({
-      include: {
-        mdi: ['*'], // (Default) Loads entire Material Design Icon set
-        devicon: ['*'], // Loads only Material Design Icon's "account" SVG
-        'simple-icons': ['*'],
-        logos: ['*'],
-      },
-    }),
-  ],
+  integrations: [icon({
+    include: {
+      mdi: ['*'], // (Default) Loads entire Material Design Icon set
+      devicon: ['*'], // Loads only Material Design Icon's "account" SVG
+      'simple-icons': ['*'],
+      logos: ['*'],
+    },
+  }), robotsTxt()],
   vite: {
     css: {
       devSourcemap: false, // Disable source maps in development
