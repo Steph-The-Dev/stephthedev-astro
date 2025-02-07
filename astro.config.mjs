da34a68f4d31cfe1fe841633b6e7402c25e6bf9c
Steph-The-Dev <69237947+Steph-The-Dev@users.noreply.github.com>
2025-02-07 09:34:10 +0100

    added 'site' to astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,19 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon({
-    include: {
-      mdi: ['*'], // (Default) Loads entire Material Design Icon set
-      devicon: ['*'], // Loads only Material Design Icon's "account" SVG
-      'simple-icons': ['*'],
-      logos: ['*'],
-    },
-  }), robotsTxt(), sitemap()],
+  site: 'https://stephthedev.de',
+  integrations: [
+    icon({
+      include: {
+        mdi: ['*'], // (Default) Loads entire Material Design Icon set
+        devicon: ['*'], // Loads only Material Design Icon's "account" SVG
+        'simple-icons': ['*'],
+        logos: ['*'],
+      },
+    }),
+    robotsTxt(),
+    sitemap(),
+  ],
   vite: {
     css: {
       devSourcemap: false, // Disable source maps in development
